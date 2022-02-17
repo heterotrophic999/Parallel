5c0cf74304c67fa90ecac6cff2e0b55d0f1bf63a
heterotrophic999 <taratutin615@gmail.com>
2022-02-17 14:19:40 +0000

    0.9s

--- Puasson.cpp
+++ Puasson.cpp
@@ -2,16 +2,16 @@
 #include <cmath>
 
 int main(int argc, char *argv[]){
+
     double tol = atof(argv[1]); 
     int size = strtol(argv[2], NULL, 10); 
     int iter_max = strtol(argv[3], NULL, 10); 
-
-    double** A = (double**)malloc((size + 2) * sizeof(double*));
-    for (int i = 0; i < size + 2; ++i) {
-        A[i] = (double*)calloc(size + 2, sizeof(double));
-    }
-
-    //double A[size+2][size+2];
+    
+    double A[size+2][size+2];
+    // double** A = (double**)malloc((size + 2) * sizeof(double*));
+    // for (int i = 0; i < size + 2; ++i) {
+    //     A[i] = (double*)calloc(size + 2, sizeof(double));
+    // }
 
     A[0][0] = 10;
     A[size+1][0] = 20;
@@ -43,24 +43,23 @@ int main(int argc, char *argv[]){
 //        std::cout << std::endl;
 //    }
 
-    double** Anew = (double**)malloc((size + 2) * sizeof(double*));
-    for (int i = 0; i < size + 2; ++i) {
-        Anew[i] = (double*)calloc(size + 2, sizeof(double));
-    }
+    // double** Anew = (double**)malloc((size + 2) * sizeof(double*));
+    // for (int i = 0; i < size + 2; ++i) {
+    //     Anew[i] = (double*)calloc(size + 2, sizeof(double));
+    // }
+    double Anew[size+2][size+2];
 
     double err = 1;
+    int iter = 0;
+    //double** p;
     #pragma acc data copy(A) create(Anew) create(err)
     {
-    //int iter_max = 1000000;
-    //double tol = 0.000001;
-
-    int iter = 0;
     while (err > tol && iter < iter_max){
         iter++;
-
         #pragma acc kernels
-        err = 0.0;
+        err = 0;
 
+        #pragma acc data present(A, Anew)
         #pragma acc kernels
         {
             #pragma acc loop independent collapse(2) reduction(max:err)
@@ -71,6 +70,10 @@ int main(int argc, char *argv[]){
             }
         }
         
+        // p = A;
+        // A = Anew;
+        // Anew = p;
+
         for (int j = 1; j < size + 1; j++) {
             for (int i = 1; i < size + 1; i++) {
                 A[i][j] = Anew[i][j];
@@ -78,7 +81,7 @@ int main(int argc, char *argv[]){
         }
         }
         if (iter%100 == 0 || iter==1)
-        #pragma acc update self(err)
+            #pragma acc update self(err)
             std::cout << iter << " " << err << std::endl;
     }
     }
